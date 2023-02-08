d0836a18fc1f84daa28a4ec97835b6f2f9d2e6a8
Pedro Marques <pedromar2010@gmail.com>
2023-02-08 00:36:06 +0000

    finished week1 and started week2

--- Week1-Docker.Postgres,SQL,Terraform/1_terraform_aws/terraform/var.tf
+++
@@ -1,8 +0,0 @@
-# var.tf is used to declare values of variables. We can either provide a default value to be used when needed or ask for value during execution.
-variable "bucket_name" {
-    default = "dtc-data-lake"
-}
-
-variable "aws_region" {
-    default = "us-east-1"
-}
--- Week1-Docker.Postgres,SQL,Terraform/pipeline.py
+++
@@ -1,7 +0,0 @@
-import pandas
-import sys
-
-print(sys.argv)
-day = sys.argv[1]
-
-print(f"Finished pipeline execution for day {day}")
